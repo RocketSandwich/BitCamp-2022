a0ae1727580d39c3d600bf444a950379b2041242
minhnhatnguyen2003 <doristrungvuong@gmail.com>
2022-04-09 05:29:22 -0400

    Hardcoded paras, cache df

--- app.py
+++ app.py
@@ -5,42 +5,24 @@ from os import path
 from PIL import Image
 import matplotlib.pyplot as plt
 
+@st.cache(persist=True)
+def read_csv_and_lowerCase_columnNames(df):
+    df = pd.read_csv('DC_propertyOpenData.csv',encoding='latin-1', low_memory = False)
+    df.columns= df.columns.str.lower()
+    return df
 
 st.title("Real Estate Price Prediction based on selection")
-df = pd.read_csv('DC_propertyOpenData.csv',encoding='latin-1', low_memory = False)
-df.columns= df.columns.str.lower()
-test = df.astype(str)
-# st.dataframe(test)
-
-#Number of bathrooms
-bathrmL = df['bathrm'].tolist()
-maxBath =  max(list(set(bathrmL)))
-minBath =  min(list(set(bathrmL)))
-
-#Number of rooms
-roomL = df['rooms'].tolist()
-maxRm =  max(list(set(roomL)))
-minRm =  min(list(set(roomL)))
-
-#Number of bedrooms
-BedrmL = df['bedrm'].tolist()
-maxBed =  max(list(set(BedrmL)))
-minBed =  min(list(set(BedrmL)))
-
-#Number of kitchens
-kitL = df['kitchens'].tolist()
-maxKit =  max(list(set(kitL)))
-minKit =  min(list(set(kitL)))
-
-
-# selection box
+#selection box
 st.write("Your preference")
-numOfRooms = st.slider("How many rooms?", minRm, maxRm)
-numOfBedrooms = st.slider("How many bedrooms?", minBed, maxBed)
-numOfBathrooms = st.slider("How many bathrooms?", minBath, maxBath)
-numOfKitchen = st.slider("How many kitchens?", minKit, maxKit)
+numOfRooms = st.slider("How many rooms?", 0, 48)
+numOfBedrooms = st.slider("How many bedrooms?", 0, 24)
+numOfBathrooms = st.slider("How many bathrooms?", 0, 14)
+numOfKitchen = st.slider("How many kitchens?", 0, 14)
 AC = st.checkbox("Have AC?")
 condition = st.selectbox('How would you like the condition of the house to be?',('Very Good','Good', 'Average'))
 
 #selection df 1
-selectDF1 = df.loc[(df['bathrm'] == numOfBathrooms)]
+# selectDF1 = df.loc[(df['bathrm'] == numOfBathrooms) &(df['rooms'] == numOfRooms) ]
+# st.dataframe(selectDF1)
+# df1 = pd.DataFrame(df, columns= ['latitude','longitude'])
+# st.map(df1)
